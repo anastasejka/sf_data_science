f897676ac80d56049567605dda1e2724e4655f6a
anastasiia_gorlenko <anastasejka@mail.ru>
2023-01-09 14:01:39 +0600

    initial commit

--- project_0/game_v2.py
+++ project_0/game_v2.py
@@ -13,7 +13,7 @@ def random_predict(number:int=1) -> int:
 
     while True:
         count += 1
-        predict_number = np.random.randint(1, 101) # предполагаемое число
+        predict_number = np.random.randint(1, 500) # предполагаемое число
         if number == predict_number:
             break # выход из цикла, если угадали
     return(count)
@@ -43,3 +43,5 @@ def score_game(random_predict) -> int:
 
 # RUN
 score_game(random_predict)
+if __name__ == '__main__':
+    score_game(random_predict)
